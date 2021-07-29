0f089877e43dafcbb5a84efc77f726a4cc5bedc5
kuz-olik <83822080+kuz-olik@users.noreply.github.com>
2021-07-29 19:15:06 +0400

    Добавила адаптивность основной страницы

--- js/main.js
+++ js/main.js
@@ -13,3 +13,15 @@ var swiper = new Swiper(".mySwiper", {
           swiper: swiper,
         },
       });
+
+const menuBtn = document.querySelector('.menu-button');
+const menu = document.querySelector('.navbar-panel');
+const menuCloseBtn = document.querySelector('.close-menu');
+
+menuBtn.addEventListener('click', () => {
+	menu.classList.add('is-open');
+});
+
+menuCloseBtn.addEventListener('click', () => {
+	menu.classList.remove('is-open');
+});
